0132f46ac8ff44622abd5cb5907704b2272f3363
Leerm206 <manh151clc@gmail.com>
2025-05-14 22:54:12 +0700

    1

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const input = document.getElementById("inputText");
-const preview = document.getElementById("preview");
+function addtask() {
+  var addTaskList = document.getElementsByClassName("Addtasklist")[0];
+  console.log(addTaskList.style);
+  if (addTaskList.style.display == "none" || addTaskList.style.display == "") {
+    addTaskList.style.display = "block";
+  } else {
+    addTaskList.style.display = "none";
+  }
+}
 
-input.addEventListener("input", function () {
-  preview.textContent = input.value;
-});
+function changedatecss(buttonday) {
+  var button = document.getElementsByClassName("day-button");
+  for (var i = 0; i < button.length; i++) {
+    button[i].classList.remove("clicked");
+  }
+  buttonday.classList.add("clicked");
+}
+
+function changeproress(checkbox) {
+  var taskitem = checkbox.parentElement;
+  if (checkbox.checked) {
+    var progress = taskitem.getElementsByClassName("progress")[0];
+    progress.innerText = "Finish";
+    taskitem.classList.add("Finish");
+  } else {
+    var progress = taskitem.getElementsByClassName("progress")[0];
+    if (taskitem.classList.contains("Overdue")) {
+      progress.innerText = "Overdue";
+    } else {
+      progress.innerText = "";
+    }
+    taskitem.classList.remove("Finish");
+  }
+}
